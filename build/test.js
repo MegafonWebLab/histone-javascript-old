2b08e70a5d08f09fde5294dfe8266850e5bc7df1
Ruslan <matveev.ruslan@gmail.com>
2012-09-29 13:17:35 -0700

    adding travis-ci configuration

---
+++ build/test.js
@@ -0,0 +1,105 @@
+load('Histone.js');
+
+const Java = JavaImporter(
+	java.io.File,
+	java.net.URI,
+	java.io.FileWriter,
+	java.io.PrintWriter,
+	java.io.FileInputStream,
+	java.io.FileOutputStream,
+	org.w3c.dom.NodeList,
+	javax.xml.parsers.DocumentBuilderFactory,
+	javax.xml.xpath.XPathFactory,
+	javax.xml.xpath.XPathConstants
+);
+
+function readDir(path, callback) {
+	var result = [];
+	if (!callback) callback = function() {};
+	var dirObj = new java.io.File(path);
+	if (dirObj.isDirectory()) {
+		var files = dirObj.list();
+		var absolutePath = (dirObj.getAbsolutePath() + '');
+		for (var fileDescr = {}, c = 0; c < files.length; c++) {
+			var filePath = absolutePath.concat('/', String(files[c]));
+			var fileObj = new java.io.File(filePath);
+			var fileName = String(fileObj.getName());
+			if (fileObj.isDirectory()) fileDescr = {
+				'type': 'folder', 'name': fileName, 'path': filePath
+			}; else if (fileObj.isFile()) fileDescr = {
+				'type': 'file', 'name': fileName, 'path': filePath
+			};
+			if (callback(fileDescr) === false) continue;
+			if (fileObj.isDirectory()) {
+				fileDescr.files = readDir(filePath, callback);
+				if (callback(fileDescr)) result.push(fileDescr);
+			} else result.push(fileDescr);
+		}
+	}
+	return result;
+};
+
+var testResult = 0;
+
+readDir('histone-acceptance-tests/src/main/acceptance/parser', function(file) {
+	var fileType = file.name.split('.').pop();
+	if (fileType !== 'json') return;
+
+	// if (file.name === 'functions.json') {
+	// 	var testSuites = readFile(file.path);
+	// 	testSuites = JSON.parse(testSuites);
+	// 	for (var i = 0; i < testSuites.length; i++) {
+	// 		var testSuite = testSuites[i];
+	// 		print('TESTING:', testSuite.name);
+	// 		for (var j = 0; j < testSuite.cases.length; j++) {
+	// 			var testCase = testSuite.cases[j];
+	// 			if (!testCase.expected) continue;
+	// 			testCase.expected = JSON.parse(testCase.expected);
+	// 		}
+	// 	}
+	// 	print(JSON.stringify(testSuites));
+	// }
+
+
+	var testSuites = readFile(file.path);
+	testSuites = JSON.parse(testSuites);
+
+	for (var i = 0; i < testSuites.length; i++) {
+		var testSuite = testSuites[i];
+		print('\n[TESTING "' + testSuite.name + '"]\n');
+		for (var j = 0; j < testSuite.cases.length; j++) {
+			var testCase = testSuite.cases[j];
+			var input = testCase.input;
+
+			if (testCase.hasOwnProperty('expected')) {
+				var expected = testCase.expected;
+				expected = JSON.stringify(expected);
+				var result = Histone(input);
+				result = JSON.stringify(result.getAST());
+				if (expected === result) {
+					print('	[SUCCESS]', input);
+				} else {
+					print('	[FAILING]', input);
+					testResult = 1;
+				}
+
+			}
+
+			else if (testCase.hasOwnProperty('exception')) {
+				// var exception = testCase.exception;
+				// try {
+				// 	Histone(input);
+				// } catch (error) {
+				// 	for (var key in exception) {
+				//
+				// 	}
+				// 	// print(JSON.stringify(result));
+				// }
+			}
+
+		}
+	}
+
+});
+
+quit(testResult);
