d9de30648c62d1338c82131c7925cbf0566e05fc
Ruslan <matveev.ruslan@gmail.com>
2012-10-01 06:25:46 -0700

    adjusting test runner to work with evaluator test cases

--- build/test.js
+++ build/test.js
@@ -58,17 +58,17 @@ function readDir(path, callback) {
 
 var testResult = 0;
 
-function doTestExpected(input, testCase) {
+function testParserExpected(input, testCase) {
 	var expected = testCase.expected;
 	expected = JSON.stringify(expected);
 	try {
 		var result = Histone(input);
 		result = JSON.stringify(result.getAST());
-		if (expected === result) return true;
+		return (expected === result);
 	} catch (e) { return false; }
 }
 
-function doTestException(input, testCase) {
+function testParserException(input, testCase) {
 	var exception = testCase.exception;
 	try { Histone(input); } catch (error) {
 		for (var key in exception) {
@@ -83,26 +83,43 @@ function doTestException(input, testCase) {
 	return false;
 }
 
+function testEvaluatorExpected(input, testCase, callback) {
+	var context = testCase.context;
+	var expected = testCase.expected;
+	try {
+		var result = false;
+		var template = Histone(input);
+		template.render(function(result) {
+			callback(expected === result);
+		}, context);
+	} catch (e) { callback(false); }
+}
+
 function printSuccess(message) {
 	var message = JSON.stringify(message);
 	message = message.substr(1);
 	message = message.substr(0, message.length - 1);
-	print('     [ SUCCESS ]', message);
+	print('     [ SUCCESS ] ', message);
 }
 
 function printFail(message) {
 	var message = JSON.stringify(message);
 	message = message.substr(1);
 	message = message.substr(0, message.length - 1);
-	print('---- [ FAILING ]', message);
+	print('---- [ FAILING ] ', message);
 }
 
-readDir('histone-acceptance-tests/src/main/acceptance/parser', function(file) {
+readDir('histone-acceptance-tests/src/main/acceptance', function(file) {
+	if (file.type === 'folder') {
+		if (file.name === 'evaluator') return true;
+		if (file.name === 'parser') return true;
+		return false;
+	}
 	var fileType = file.name.split('.').pop();
 	if (fileType !== 'json') return;
 
-	//
-	// if (file.name === 'functions.json') {
+
+	// if (file.name === 'constructs.json') {
 	// 	var testSuites = readFile(file.path);
 	// 	testSuites = JSON.parse(testSuites);
 	// 	for (var i = 0; i < testSuites.length; i++) {
@@ -110,8 +127,8 @@ readDir('histone-acceptance-tests/src/main/acceptance/parser', function(file) {
 	// 		print('TESTING:', testSuite.name);
 	// 		for (var j = 0; j < testSuite.cases.length; j++) {
 	// 			var testCase = testSuite.cases[j];
-	// 			if (!testCase.expected) continue;
-	// 			testCase.expected = JSON.parse(testCase.expected);
+	// 			if (!testCase.context) continue;
+	// 			testCase.context = JSON.parse(testCase.context);
 	// 		}
 	// 	}
 	// 	print(JSON.stringify(testSuites));
@@ -127,15 +144,26 @@ readDir('histone-acceptance-tests/src/main/acceptance/parser', function(file) {
 			var testCase = testSuite.cases[j];
 			var input = testCase.input;
 			if (testCase.hasOwnProperty('expected')) {
-				if (doTestExpected(input, testCase)) {
-					printSuccess(input);
-				} else {
-					testResult = 1;
-					printFail(input);
-				}
+				if (testCase.expected instanceof Array) {
+					if (testParserExpected(input, testCase)) {
+						printSuccess(input);
+					} else {
+						testResult = 1;
+						printFail(input);
+					}
+				} else testEvaluatorExpected(
+					input, testCase, function(result) {
+						if (result) {
+							printSuccess(input);
+						} else {
+							testResult = 1;
+							printFail(input);
+						}
+					}
+				);
 			}
 			else if (testCase.hasOwnProperty('exception')) {
-				if (doTestException(input, testCase)) {
+				if (testParserException(input, testCase)) {
 					printSuccess(input);
 				} else {
 					testResult = 1;
