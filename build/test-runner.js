6393d377e73a99f9b9824c0ff97a739c70379775
Ruslan Matveev <matveev.ruslan@gmail.com>
2012-12-06 12:52:15 +0400

    trying to fix the build

--- build/test-runner.js
+++ build/test-runner.js
@@ -263,9 +263,6 @@ if (Files.isDir(testPath)) {
 		if (file.type === 'folder') {
 			return (file.name !== 'testresources');
 		} else if (file.type === 'file') {
-			// if (file.name !== 'examples3.json') {
-				// return false;
-			// }
 		}
 		files.push(file.path);
 	});
