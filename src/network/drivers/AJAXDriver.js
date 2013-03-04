bc3434cd4b41c1638183af911c43c76ff630b1d9
Ruslan Matveev <matveev.ruslan@gmail.com>
2013-03-04 15:08:10 +0400

    - improve XMLHTTPFactory detection - use ret callback instead of fail and success - get rid of data uri processing inside the driver

--- src/network/drivers/AJAXDriver.js
+++ src/network/drivers/AJAXDriver.js
@@ -17,33 +17,28 @@
 
 define(['../../Utils'], function(Utils) {
 
+	var XMLHttpFactory = null;
+
 	var XMLHttpFactories = [
-		function () {return new XMLHttpRequest()},
-		function () {return new ActiveXObject('Msxml2.XMLHTTP')},
-		function () {return new ActiveXObject('Msxml3.XMLHTTP')},
-		function () {return new ActiveXObject('Microsoft.XMLHTTP')}
+		function() { return new XMLHttpRequest(); },
+		function() { return new ActiveXObject('Msxml2.XMLHTTP'); },
+		function() { return new ActiveXObject('Msxml3.XMLHTTP'); },
+		function() { return new ActiveXObject('Microsoft.XMLHTTP'); }
 	];
 
 	function createXMLHTTPObject() {
-		var xmlhttp = false;
+		if (XMLHttpFactory) return XMLHttpFactory;
 		for (var c = 0; c < XMLHttpFactories.length; c++) {
-			try {
-				xmlhttp = XMLHttpFactories[c]();
-			} catch (e) { continue; }
-			break;
+			try { return XMLHttpFactory = XMLHttpFactories[c](); }
+			catch (exception) {}
 		}
-		return xmlhttp;
 	}
 
-	function doRequest(requestURI, success, fail, requestProps) {
+	function doXMLHTTPRequest(requestURI, ret, requestProps) {
 		var request = createXMLHTTPObject();
-		if (!request) return fail();
-
-		var postData = null;
-
+		if (!request) return ret();
 		try {
-			request.open(requestProps.method, requestURI, true);
-
+			request.open(requestProps.method, requestURI, requestProps.async);
 			var requestHeaders = requestProps.headers;
 			for (var headerName in requestHeaders) {
 				if (requestHeaders.hasOwnProperty(headerName)) {
@@ -52,17 +47,14 @@ define(['../../Utils'], function(Utils) {
 					);
 				}
 			}
-
 			request.onreadystatechange = function() {
 				if (request.readyState !== 4) return;
 				var status = request.status;
-				if (status > 399 && status < 600) return fail();
-				success(request.responseText);
+				if (status > 399 && status < 600) return ret();
+				ret(request.responseText);
 			};
-
 			request.send(requestProps.data);
-
-		} catch (e) { fail(); }
+		} catch (exception) { ret(); }
 	}
 
 	function removeCallback(callbackId) {
@@ -71,67 +63,26 @@ define(['../../Utils'], function(Utils) {
 		scriptElement.parentNode.removeChild(scriptElement);
 	}
 
-	function doJSONPRequest(requestURI, success, fail, requestProps) {
-
-		var requestQuery = [];
+	function doJSONPRequest(requestURI, ret, requestProps) {
 		var requestURI = Utils.uri.parse(requestURI);
-		var requestParams = Utils.uri.parseQuery(requestURI.query);
-		var callbackId = Utils.uniqueId('callback');
-		requestParams.callback = callbackId;
-
-		for (var paramName in requestParams) {
-			if (requestParams.hasOwnProperty(paramName)) {
-				requestQuery.push(paramName +
-					'=' + requestParams[paramName]
-				);
-			}
-		}
-
-		requestURI = (
-			(requestURI.scheme ? requestURI.scheme + '://' : '') +
-			(requestURI.authority ? requestURI.authority : '') +
-			(requestURI.path ? requestURI.path : '') +
-			(requestQuery.length ? '?' + requestQuery.join('&') : '') +
-			(requestURI.fragment ? '#' + requestURI.fragment : '')
-		);
-
+		var query = Utils.uri.parseQuery(requestURI.query);
+		if (!query.callback) query.callback = Utils.uniqueId('callback')
+		requestURI.query = Utils.uri.formatQuery(query);
+		requestURI = Utils.uri.format(requestURI);
+		var callbackId = query.callback;
 		var cleanup = removeCallback.bind(this, callbackId);
-
-		window[callbackId] = function(response) {
-			cleanup();
-			success(response);
-		};
-
-
+		window[callbackId] = function(response) { cleanup(); ret(response); };
 		var scriptElement = document.createElement('script');
 		scriptElement.setAttribute('id', callbackId);
 		scriptElement.setAttribute('src', requestURI);
 		scriptElement.setAttribute('type', 'text/javascript');
-
-		scriptElement.onerror = function() {
-			cleanup();
-			fail();
-		};
-
-		// scriptElement.onerror =
-		// scriptElement.onreadystatechange =
-		// removeCallback.bind(this, callbackId);
-
+		scriptElement.onerror = function() { cleanup(); ret(); };
 		document.getElementsByTagName('head')[0].appendChild(scriptElement);
 	}
 
-	return function(requestURI, success, fail, requestProps, isJSONP) {
-		var requestObj = Utils.uri.parse(requestURI);
-		if (requestObj.scheme === 'data') {
-			requestObj = Utils.uri.parseData(requestObj.path);
-			if (requestObj.encoding === 'base64') {
-				var requestData = requestObj.data;
-				requestData = atob(requestData);
-				success(requestData);
-			} else fail();
-		}
-		else if (!isJSONP) doRequest(requestURI, success, fail, requestProps);
-		else doJSONPRequest(requestURI, success, fail, requestProps);
+	return function(requestURI, ret, requestProps, isJSONP) {
+		if (!isJSONP) doXMLHTTPRequest(requestURI, ret, requestProps);
+		else doJSONPRequest(requestURI, ret, requestProps);
 	};
 
 });
